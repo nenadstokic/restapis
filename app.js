728817806691da061c30307d26f430c21fcfb933
Nenad <nstokic@gmail.com>
2020-03-11 01:31:03 +0100

    Testing with postman

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const feedRoutes = require("./routes/feed");
+
+const app = express();
+
+//parse incoming data as json
+app.use(bodyParser.json()); // application/json
+
+app.use("/feed", feedRoutes);
+
+app.listen(8080);
---
+++ controllers/feed.js
@@ -0,0 +1,15 @@
+exports.getPosts = (req, res, next) => {
+  res.status(200).json({
+    posts: [{ title: "First post", content: "This is the first post, yay!" }]
+  });
+};
+
+exports.createPost = (req, res, next) => {
+  const title = req.body.title;
+  const content = req.body.content;
+  // Create post in DB
+  res.status(201).json({
+    message: "Post created successfully!",
+    post: { id: Date.now(), title: title, content: content }
+  });
+};
