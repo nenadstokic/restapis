a7cc055fb90328708f385e96b2d2921dd0e0fa98
Nenad Stokic <nstokic@gmail.com>
2020-03-11 19:28:36 +0100

    CORS headers added

--- app.js
+++ app.js
@@ -7,6 +7,16 @@ const app = express();
 //parse incoming data as json
 app.use(bodyParser.json()); // application/json
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
 app.use("/feed", feedRoutes);
 
 app.listen(8080);
